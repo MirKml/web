5a7e691c83b2006f8deca1c147945a3de2de6a6b
Kubelik <koubel@seznam.cz>
2016-10-18 23:57:11 +0200

    comment on article bug, debug, remote ip tracy, docker timezone

--- webapp/bootstrap.php
+++ webapp/bootstrap.php
@@ -4,7 +4,7 @@ require __DIR__ . '/../vendor/autoload.php';
 $configurator = new Nette\Configurator;
 
 // "192.168.56.1" is the IP for the virtualbox host IP address
-$configurator->setDebugMode(["127.0.0.1", "192.168.56.2", "94.112.21.42"]); // enable for your remote IP
+$configurator->setDebugMode(["127.0.0.1", "192.168.56.1", "94.112.21.42"]); // enable for your remote IP
 // enable only when there isn't obvious error reason from normal apache log
 // because tracy logs aren't rotated
 $configurator->enableDebugger(__DIR__ . '/../log');
